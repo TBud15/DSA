4f4d48a20b0b073788d7a41a95d0c742b3ebc9df
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-12-11 23:42:48 -0500

    time complexity examples

---
+++ timeComp1.py
@@ -0,0 +1,12 @@
+# # Example of O(1)
+# array = [0,1,2,3,4]
+
+# # retrieve the number found at index location 3 
+# print(array[3]) 
+
+# Example of O(n)
+array = [0,1,2,3,4,5,6,7,8,9,10]
+
+if 5 in array:
+    print("five is still alive")
+
