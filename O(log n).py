86cda1322afd0e8c4159db102b6c87a9538e201b
Fluffy1908 <pro1shinov.tima@gmail.com>
2023-12-14 11:15:47 -0500

    formatted

--- O(log n).py
+++ O(log n).py
@@ -1,27 +1,3 @@
-# # Example of O(1)
-# array = [0,1,2,3,4]
-
-# # retrieve the number found at index location 3 
-# print(array[3]) 
-
-
-
-
-
-# # Example of O(n)
-# array = [0,1,2,3,4,5,6,7,8,9,10]
-
-# if 5 in array:
-#     print("five is still alive")
-
-
-
-
-
-
-
-
-#Example of O(Log n)
 array = [0,1,2,3,4,6,7,8,9,10]
 
 print("##Step One")
